8504b3c72884a3288a5542dc3953b5439ae6da5c
agintegrated <dwolfe@agintegrated.com>
2018-01-22 15:55:32 -0500

    Updated the Marshaller to use the same exact regex logic as the unmarshaller.  Suppressed the null attribute output in the NullConverter.

--- XStream/Converters/NullConverter.cs
+++ XStream/Converters/NullConverter.cs
@@ -6,8 +6,10 @@ namespace xstream.Converters {
             return false;
         }
 
-        public void ToXml(object value, XStreamWriter writer, MarshallingContext context) {
-            writer.WriteAttribute(Attributes.Null, true.ToString());
+        public void ToXml(object value, XStreamWriter writer, MarshallingContext context)
+        {
+            // Suppress null output
+            //  writer.WriteAttribute(Attributes.Null, true.ToString());
         }
 
         public object FromXml(XStreamReader reader, UnmarshallingContext context) {
